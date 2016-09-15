c524af0d6524a7be683176a3bb9c90f505cff9c2
Ran Gurung <rangurung.rsr@gmail.com>
2016-09-15 17:45:47 -0400

    Restful services with springmvc

---
+++ src/main/java/com/springmvc/configuration/SpringInitializer.java
@@ -0,0 +1,28 @@
+package com.springmvc.configuration;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {SpringConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	protected Filter[] getServletFilters(){
+		Filter[] singleton = {new CORSFilter()};
+		return singleton;
+	}
+}
